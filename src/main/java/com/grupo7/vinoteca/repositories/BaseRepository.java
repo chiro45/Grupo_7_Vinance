0d602e6d62924959f5f8bb1ff1c3c6721f018da4
chiro45 <luciano.chiroli45@gmail.com>
2022-10-15 16:07:36 -0300

    create BaseRepository

---
+++ src/main/java/com/grupo7/vinoteca/repositories/BaseRepository.java
@@ -0,0 +1,14 @@
+package com.grupo7.vinoteca.repositories;
+
+
+import com.grupo7.vinoteca.entities.Base;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.NoRepositoryBean;
+
+import java.io.Serializable;
+
+@NoRepositoryBean
+public interface BaseRepository<E extends Base, ID extends Serializable> extends JpaRepository<E, ID> {
+
+
+}
