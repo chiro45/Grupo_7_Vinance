a46765f33883470ff4f6b513de30f2e97c266685
chiro45 <luciano.chiroli45@gmail.com>
2022-10-15 17:06:38 -0300

    SellerController

--- src/main/java/com/grupo7/vinoteca/controllers/SellerController.java
+++ src/main/java/com/grupo7/vinoteca/controllers/SellerController.java
@@ -1,70 +1,14 @@
 package com.grupo7.vinoteca.controllers;
-
+import com.grupo7.vinoteca.controllers.BaseControllerImp.BaseControllerImpl;
 import com.grupo7.vinoteca.entities.Seller;
-import com.grupo7.vinoteca.entities.User;
-import com.grupo7.vinoteca.services.SellerService;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
+import com.grupo7.vinoteca.services.Implementation.SellerServiceImpl;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @CrossOrigin(origins = "*")
 @RequestMapping(path = "/sellers")
-public class SellerController {
 
-
-    private SellerService service;
-
-
-    public SellerController(SellerService sellerService){
-        this.service = sellerService;
-    }
-    @GetMapping("/get")
-    public ResponseEntity<?> getAll(){
-        try {
-            return ResponseEntity.status(HttpStatus.OK).body(service.findAll());
-        }catch (Exception e){
-            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("{\"error\":\"Error, por favor intente nuevamente...\"");
-
-        }
-    }
-    @GetMapping("/{id}")
-    public ResponseEntity<?> getOne(@PathVariable Long id){
-        try{
-            return ResponseEntity.status(HttpStatus.OK).body(service.findById(id));
-        }catch (Exception e){
-            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("{\"error\":\"Error, por favor intente nuevamente...\"");
-        }
-    }
-
-    @PostMapping("")
-    public ResponseEntity<?> save(@RequestBody Seller entity){
-        try{
-            return ResponseEntity.status(HttpStatus.OK).body(service.save(entity));
-        }catch (Exception e){
-            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("{\"error\":\"Error, por favor intente nuevamente...\"");
-        }
-    }
-
-    @PutMapping("/{id}")
-    public ResponseEntity<?> update(@RequestBody Seller entity, @PathVariable Long id){
-        try{
-            return ResponseEntity.status(HttpStatus.OK).body(service.update(entity, id));
-        }catch (Exception e){
-            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("{\"error\":\"Error, por favor intente nuevamente...\"");
-        }
-    }
-
-    @DeleteMapping("/{id}")
-    public ResponseEntity<?> delete(@PathVariable Long id){
-        try{
-            return ResponseEntity.status(HttpStatus.NO_CONTENT).body(service.delete(id));
-        }catch (Exception e){
-            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("{\"error\":\"Error, por favor intente nuevamente...\"");
-        }
-    }
-
-
+public class SellerController extends BaseControllerImpl<Seller, SellerServiceImpl> {
 
 
 }
