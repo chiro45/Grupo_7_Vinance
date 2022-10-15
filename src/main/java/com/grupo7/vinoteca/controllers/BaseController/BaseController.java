b2cdc21d663a922efc3362239266335155f1e5c5
chiro45 <luciano.chiroli45@gmail.com>
2022-10-15 17:05:51 -0300

    Base Controoller

---
+++ src/main/java/com/grupo7/vinoteca/controllers/BaseController/BaseController.java
@@ -0,0 +1,22 @@
+package com.grupo7.vinoteca.controllers.BaseController;
+
+import com.grupo7.vinoteca.entities.Base;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+
+import java.io.Serializable;
+
+public interface BaseController<E extends Base,ID extends Serializable> {
+
+    public ResponseEntity<?> getAll();
+
+    public ResponseEntity<?> getOne(@PathVariable ID id);
+
+    public ResponseEntity<?> save(@RequestBody E entity);
+
+    public ResponseEntity<?> update( @RequestBody E entity, @PathVariable Long id);
+
+    public ResponseEntity<?> delete(@PathVariable ID id);
+
+}
