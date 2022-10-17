bfad96dedaa603d1705c8a289f76fc8be2394d70
Laujo11 <103857908+Laujo11@users.noreply.github.com>
2022-10-17 09:01:46 -0300

    Update AccessoryServiceImpl.java

--- src/main/java/com/grupo7/vinoteca/services/Implementation/AccessoryServiceImpl.java
+++ src/main/java/com/grupo7/vinoteca/services/Implementation/AccessoryServiceImpl.java
@@ -6,7 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
-public class AccessoryServiceImpl extends BaseServiceImp<Accessory, Long> {
+public class AccessoryServiceImpl extends BaseServiceImp<Accessory, Long> implements AccesoryService {
 
     @Autowired
     private AccessoryRepository accessoryRepository;
